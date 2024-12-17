84eec053a3dbaf492bd9e57cec15c6d1f687e3ee
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-12-17 17:23:37 -0500

    Port mipmapping improvement

--- forge122/src/main/java/org/taumc/celeritas/mixin/features/mipmaps/TextureUtilMixin.java
+++ forge122/src/main/java/org/taumc/celeritas/mixin/features/mipmaps/TextureUtilMixin.java
@@ -1,19 +1,19 @@
-package org.embeddedt.embeddium.impl.mixin.features.textures.mipmaps;
+package org.taumc.celeritas.mixin.features.mipmaps;
 
-import net.minecraft.client.renderer.texture.MipmapGenerator;
+import net.minecraft.client.renderer.texture.TextureUtil;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 
 import static org.embeddedt.embeddium.impl.texture.MipmapHelper.weightedAverageColor;
 
-@Mixin(MipmapGenerator.class)
-public class MipmapHelperMixin {
+@Mixin(TextureUtil.class)
+public class TextureUtilMixin {
     /**
      * @author coderbot
      * @reason replace the vanilla blending function with our improved function
      */
     @Overwrite
-    private static int alphaBlend(int one, int two, int three, int four, boolean checkAlpha) {
+    private static int blendColors(int one, int two, int three, int four, boolean checkAlpha) {
         // First blend horizontally, then blend vertically.
         //
         // This works well for the case where our change is the most impactful (grass side overlays)
