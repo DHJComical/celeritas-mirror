e72fe625ba2fa3f14ff37d582a9c1f4205079093
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-12-26 12:41:44 -0500

    Thanks Mojang

--- modern/src/main/java/org/embeddedt/embeddium/impl/util/ModernBlockPosUtil.java
+++ modern/src/main/java/org/embeddedt/embeddium/impl/util/ModernBlockPosUtil.java
@@ -5,7 +5,10 @@ import net.minecraft.core.Direction;
 
 public class ModernBlockPosUtil {
     public static BlockPos.MutableBlockPos setWithOffset(BlockPos.MutableBlockPos dest, BlockPos pos, Direction offset) {
+        //? if <1.21.4 {
         var offsetVector = offset.getNormal();
+        //?} else
+        /*var offsetVector = offset.getUnitVec3i();*/
         dest.set(pos.getX() + offsetVector.getX(), pos.getY() + offsetVector.getY(), pos.getZ() + offsetVector.getZ());
         return dest;
     }
