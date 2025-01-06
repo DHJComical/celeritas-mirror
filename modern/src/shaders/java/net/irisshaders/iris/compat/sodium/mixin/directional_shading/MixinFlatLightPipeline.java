1e525a020614b28cc2afcd48e36ea7c59bddb3ff
embeddedt <42941056+embeddedt@users.noreply.github.com>
2025-01-06 16:37:15 -0500

    Fix grass shading being very wrong

--- modern/src/shaders/java/net/irisshaders/iris/compat/sodium/mixin/directional_shading/MixinFlatLightPipeline.java
+++ modern/src/shaders/java/net/irisshaders/iris/compat/sodium/mixin/directional_shading/MixinFlatLightPipeline.java
@@ -1,5 +1,7 @@
 package net.irisshaders.iris.compat.sodium.mixin.directional_shading;
 
+import com.llamalad7.mixinextras.sugar.Local;
+import org.embeddedt.embeddium.impl.model.light.data.QuadLightData;
 import org.embeddedt.embeddium.impl.model.light.flat.FlatLightPipeline;
 import net.irisshaders.iris.shaderpack.materialmap.WorldRenderingSettings;
 import org.spongepowered.asm.mixin.Mixin;
@@ -7,12 +9,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Arrays;
+
 @Mixin(FlatLightPipeline.class)
 public class MixinFlatLightPipeline {
-	@Inject(method = "calculate", at = @At(value = "INVOKE", target = "Ljava/util/Arrays;fill([II)V", ordinal = 0, shift = At.Shift.AFTER), cancellable = true)
-	private void iris$disableDirectionalShading(CallbackInfo ci) {
+	@Inject(method = "calculate", at = @At(value = "RETURN"))
+	private void iris$disableDirectionalShading(CallbackInfo ci, @Local(ordinal = 0, argsOnly = true) QuadLightData out) {
 		if (WorldRenderingSettings.INSTANCE.shouldDisableDirectionalShading()) {
-			ci.cancel();
+            Arrays.fill(out.br, 1.0f);
 		}
 	}
 }
