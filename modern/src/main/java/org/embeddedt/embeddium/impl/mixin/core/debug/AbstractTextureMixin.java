fa01863195dff901f2f9ff6d162b214d5609f0f5
embeddedt <42941056+embeddedt@users.noreply.github.com>
2025-01-13 10:38:24 -0500

    Name textures in RenderDoc

---
+++ modern/src/main/java/org/embeddedt/embeddium/impl/mixin/core/debug/AbstractTextureMixin.java
@@ -0,0 +1,36 @@
+package org.embeddedt.embeddium.impl.mixin.core.debug;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.renderer.texture.AbstractTexture;
+import org.embeddedt.embeddium.impl.gl.debug.GLDebug;
+import org.embeddedt.embeddium.impl.render.texture.NameableTexture;
+import org.lwjgl.opengl.GL43C;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(AbstractTexture.class)
+public class AbstractTextureMixin implements NameableTexture {
+    @Shadow
+    protected int id;
+    @Unique
+    private String celeritas$name;
+
+    @ModifyExpressionValue(method = "getId", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/TextureUtil;generateTextureId()I"))
+    private int assignName(int id) {
+        if (id != -1 && celeritas$name != null) {
+            GLDebug.nameObject(GL43C.GL_TEXTURE, id, celeritas$name);
+        }
+        return id;
+    }
+
+    @Override
+    public void celeritas$setName(String name) {
+        this.celeritas$name = name;
+        if (RenderSystem.isOnRenderThread() && this.id != -1) {
+            GLDebug.nameObject(GL43C.GL_TEXTURE, this.id, name);
+        }
+    }
+}
