59f4e63f3ac30ea6835cd6b7385ef0600cd052bb
embeddedt <42941056+embeddedt@users.noreply.github.com>
2025-01-22 11:46:15 -0500

    Fix NeoForge (particle engine mixin crash)

--- modern/src/main/java/org/embeddedt/embeddium/impl/mixin/features/render/particle/ParticleEngineMixin.java
+++ modern/src/main/java/org/embeddedt/embeddium/impl/mixin/features/render/particle/ParticleEngineMixin.java
@@ -19,7 +19,7 @@ public class ParticleEngineMixin {
             //? if forge
             "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource$BufferSource;Lnet/minecraft/client/renderer/LightTexture;Lnet/minecraft/client/Camera;FLnet/minecraft/client/renderer/culling/Frustum;)V"
             //? if neoforge
-            /*"render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource$BufferSource;Lnet/minecraft/client/renderer/LightTexture;Lnet/minecraft/client/Camera;FLnet/minecraft/client/renderer/culling/Frustum;)V"*/
+            /*"render(Lnet/minecraft/client/renderer/LightTexture;Lnet/minecraft/client/Camera;FLnet/minecraft/client/renderer/culling/Frustum;Ljava/util/function/Predicate;)V"*/
     }, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/culling/Frustum;isVisible(Lnet/minecraft/world/phys/AABB;)Z"))
     private boolean celeritas$useSectionVisibility(Frustum instance, AABB aabb) {
         var renderer = CeleritasWorldRenderer.instanceNullable();
