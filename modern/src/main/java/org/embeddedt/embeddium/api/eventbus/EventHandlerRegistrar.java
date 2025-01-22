0f4f599915e65724e3f0cf8eb14fd114a638ed1a
embeddedt <42941056+embeddedt@users.noreply.github.com>
2025-01-22 11:59:22 -0500

    Actually fire events on the Neo bus, not just ours

--- modern/src/main/java/org/embeddedt/embeddium/api/eventbus/EventHandlerRegistrar.java
+++ modern/src/main/java/org/embeddedt/embeddium/api/eventbus/EventHandlerRegistrar.java
@@ -3,6 +3,9 @@ package org.embeddedt.embeddium.api.eventbus;
 //? if forge
 import net.minecraftforge.common.MinecraftForge;
 
+//? if neoforge
+/*import net.neoforged.neoforge.common.NeoForge;*/
+
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
@@ -45,7 +48,10 @@ public class EventHandlerRegistrar<T extends EmbeddiumEvent> {
     private static <T extends EmbeddiumEvent> boolean postPlatformSpecificEvent(T event) {
         //? if forge {
         return MinecraftForge.EVENT_BUS.post(event);
-        //?} else {
+        //?} else if neoforge {
+        /*NeoForge.EVENT_BUS.post(event);
+        return event.isCanceled();
+        *///?} else {
         /*return false;
         *///?}
     }
