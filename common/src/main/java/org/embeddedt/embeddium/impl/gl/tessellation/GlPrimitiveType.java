789750a7bbe489584f9dc9c199da09e777c1e8e8
embeddedt <42941056+embeddedt@users.noreply.github.com>
2025-02-01 14:47:35 -0500

    Add PATCHES primitive type

--- common/src/main/java/org/embeddedt/embeddium/impl/gl/tessellation/GlPrimitiveType.java
+++ common/src/main/java/org/embeddedt/embeddium/impl/gl/tessellation/GlPrimitiveType.java
@@ -1,9 +1,11 @@
 package org.embeddedt.embeddium.impl.gl.tessellation;
 
 import org.lwjgl.opengl.GL20C;
+import org.lwjgl.opengl.GL43C;
 
 public enum GlPrimitiveType {
-    TRIANGLES(GL20C.GL_TRIANGLES);
+    TRIANGLES(GL20C.GL_TRIANGLES),
+    PATCHES(GL43C.GL_PATCHES);
 
     private final int id;
 
