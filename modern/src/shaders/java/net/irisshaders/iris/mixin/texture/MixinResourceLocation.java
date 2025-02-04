09fbffc78704fa1c0b860c88a36f83472fe33f68
embeddedt <42941056+embeddedt@users.noreply.github.com>
2025-02-03 19:35:34 -0500

    Avoid allocations in ResourceLocation mixin

--- modern/src/shaders/java/net/irisshaders/iris/mixin/texture/MixinResourceLocation.java
+++ modern/src/shaders/java/net/irisshaders/iris/mixin/texture/MixinResourceLocation.java
@@ -1,24 +1,19 @@
 package net.irisshaders.iris.mixin.texture;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = ResourceLocation.class, priority = 1010)
 public class MixinResourceLocation {
-	@Inject(method = "isValidPath", at = @At("HEAD"), cancellable = true)
-	private static void iris$blockDUMMY(String string, CallbackInfoReturnable<Boolean> cir) {
-		if (string.equals("DUMMY")) {
-			cir.setReturnValue(false);
-		}
-	}
+	@ModifyReturnValue(method = "isValidPath", at = @At("RETURN"))
+	private static boolean iris$blockDUMMY(boolean original, String string) {
+        return original && !string.equals("DUMMY");
+    }
 
-	@Inject(method = "validPathChar", at = @At("HEAD"), cancellable = true)
-	private static void iris$allowInvalidPaths(char c, CallbackInfoReturnable<Boolean> cir) {
-		if (c >= 'A' && c <= 'Z') {
-			cir.setReturnValue(true);
-		}
-	}
+	@ModifyReturnValue(method = "validPathChar", at = @At("RETURN"))
+	private static boolean iris$allowInvalidPaths(boolean original, char c) {
+        return original || (c >= 'A' && c <= 'Z');
+    }
 }
