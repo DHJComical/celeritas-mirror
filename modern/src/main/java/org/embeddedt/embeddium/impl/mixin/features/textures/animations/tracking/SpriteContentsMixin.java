7c36d172f58e170b07e673575f9e230072727207
embeddedt <42941056+embeddedt@users.noreply.github.com>
2025-02-08 15:42:17 -0500

    Merge shader sprite animation hook into main one to avoid allocation

--- modern/src/main/java/org/embeddedt/embeddium/impl/mixin/features/textures/animations/tracking/SpriteContentsMixin.java
+++ modern/src/main/java/org/embeddedt/embeddium/impl/mixin/features/textures/animations/tracking/SpriteContentsMixin.java
@@ -1,6 +1,8 @@
 package org.embeddedt.embeddium.impl.mixin.features.textures.animations.tracking;
 
 //? if >=1.20 {
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import org.embeddedt.embeddium.api.render.texture.SpriteUtil;
 import org.embeddedt.embeddium.impl.render.texture.SpriteContentsExtended;
 import net.minecraft.client.renderer.texture.SpriteContents;
 import org.jetbrains.annotations.Nullable;
@@ -22,6 +24,19 @@ public abstract class SpriteContentsMixin implements SpriteContentsExtended {
     @Override
     public void sodium$setActive(boolean value) {
         this.active = value;
+        //? if shaders {
+        net.irisshaders.iris.texture.pbr.PBRSpriteHolder pbrHolder = ((net.irisshaders.iris.texture.pbr.SpriteContentsExtension) this).getPBRHolder();
+        if (pbrHolder != null) {
+            TextureAtlasSprite normalSprite = pbrHolder.getNormalSprite();
+            TextureAtlasSprite specularSprite = pbrHolder.getSpecularSprite();
+            if (normalSprite != null) {
+                SpriteUtil.markSpriteActive(normalSprite);
+            }
+            if (specularSprite != null) {
+                SpriteUtil.markSpriteActive(specularSprite);
+            }
+        }
+        //? }
     }
 
     @Override
