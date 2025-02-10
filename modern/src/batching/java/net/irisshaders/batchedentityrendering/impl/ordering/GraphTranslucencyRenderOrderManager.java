248e6f2c161f4dd769784175f3d4c6c9a5176634
embeddedt <42941056+embeddedt@users.noreply.github.com>
2025-02-10 16:19:33 -0500

    Use fastutil maps for GraphTranslucencyRenderOrderManager

--- modern/src/batching/java/net/irisshaders/batchedentityrendering/impl/ordering/GraphTranslucencyRenderOrderManager.java
+++ modern/src/batching/java/net/irisshaders/batchedentityrendering/impl/ordering/GraphTranslucencyRenderOrderManager.java
@@ -1,5 +1,7 @@
 package net.irisshaders.batchedentityrendering.impl.ordering;
 
+import it.unimi.dsi.fastutil.objects.Object2IntLinkedOpenHashMap;
+import it.unimi.dsi.fastutil.objects.Object2ObjectLinkedOpenHashMap;
 import net.irisshaders.iris.vendored.digraph.Digraph;
 import net.irisshaders.iris.vendored.digraph.Digraphs;
 import net.irisshaders.iris.vendored.digraph.MapDigraph;
@@ -17,6 +19,7 @@ import java.util.EnumMap;
 import java.util.List;
 
 public class GraphTranslucencyRenderOrderManager implements RenderOrderManager {
+    private static final TransparencyType[] TRANSPARENCY_TYPES = TransparencyType.values();
 	private final FeedbackArcSetProvider feedbackArcSetProvider;
 	private final EnumMap<TransparencyType, Digraph<RenderType>> types;
 	private final EnumMap<TransparencyType, RenderType> currentTypes;
@@ -27,10 +30,18 @@ public class GraphTranslucencyRenderOrderManager implements RenderOrderManager {
 		types = new EnumMap<>(TransparencyType.class);
 		currentTypes = new EnumMap<>(TransparencyType.class);
 
-		for (TransparencyType type : TransparencyType.values()) {
-			types.put(type, new MapDigraph<>());
-		}
+        initializeTransparencyDigraphs();
 	}
+
+    private void initializeTransparencyDigraphs() {
+        for (TransparencyType type : TRANSPARENCY_TYPES) {
+            types.put(type, makeDigraph());
+        }
+    }
+
+    private MapDigraph<RenderType> makeDigraph() {
+        return new MapDigraph<>(Object2ObjectLinkedOpenHashMap::new, source -> new Object2IntLinkedOpenHashMap<>());
+    }
 
 	private static TransparencyType getTransparencyType(RenderType type) {
 		while (type instanceof WrappableRenderType) {
@@ -96,15 +107,13 @@ public class GraphTranslucencyRenderOrderManager implements RenderOrderManager {
 		// TODO: Is reallocation efficient?
 		types.clear();
 
-		for (TransparencyType type : TransparencyType.values()) {
-			types.put(type, new MapDigraph<>());
-		}
+		initializeTransparencyDigraphs();
 	}
 
 	@Override
 	public void resetType(TransparencyType type) {
 		// TODO: Is reallocation efficient?
-		types.put(type, new MapDigraph<>());
+		types.put(type, makeDigraph());
 	}
 
 	public List<RenderType> getRenderOrder() {
@@ -117,27 +126,40 @@ public class GraphTranslucencyRenderOrderManager implements RenderOrderManager {
 		List<RenderType> allLayers = new ArrayList<>(layerCount);
 
 		for (Digraph<RenderType> graph : types.values()) {
-			// TODO: Make sure that FAS can't become a bottleneck!
-			// Running NP-hard algorithms in a real time rendering loop might not be an amazing idea.
-			// This shouldn't be necessary in sane scenes, though, and if there aren't cycles,
-			// then this *should* be relatively inexpensive, since it'll bail out and return an empty set.
-			FeedbackArcSet<RenderType> arcSet =
-				feedbackArcSetProvider.getFeedbackArcSet(graph, graph, FeedbackArcSetPolicy.MIN_WEIGHT);
 
-			if (arcSet.getEdgeCount() > 0) {
-				// This means that our dependency graph had cycles!!!
-				// This is very weird and isn't expected - but we try to handle it gracefully anyways.
+            // Hoist trivial check for no cycles to avoid allocation of an empty feedback arc set in a lot of cases.
+            if (!Digraphs.isTriviallyAcyclic(graph)) {
+                // TODO: Make sure that FAS can't become a bottleneck!
+                // Running NP-hard algorithms in a real time rendering loop might not be an amazing idea.
+                // This shouldn't be necessary in sane scenes, though, and if there aren't cycles,
+                // then this *should* be relatively inexpensive, since it'll bail out and return an empty set.
+                FeedbackArcSet<RenderType> arcSet =
+                        feedbackArcSetProvider.getFeedbackArcSet(graph, graph, FeedbackArcSetPolicy.MIN_WEIGHT);
 
-				// Our feedback arc set algorithm finds some dependency links that can be removed hopefully
-				// without disrupting the overall order too much. Hopefully it isn't too slow!
-				for (RenderType source : arcSet.vertices()) {
-					for (RenderType target : arcSet.targets(source)) {
-						graph.remove(source, target);
-					}
-				}
-			}
+                if (arcSet.getEdgeCount() > 0) {
+                    // This means that our dependency graph had cycles!!!
+                    // This is very weird and isn't expected - but we try to handle it gracefully anyways.
 
-			allLayers.addAll(Digraphs.toposort(graph, false));
+                    // Our feedback arc set algorithm finds some dependency links that can be removed hopefully
+                    // without disrupting the overall order too much. Hopefully it isn't too slow!
+                    for (RenderType source : arcSet.vertices()) {
+                        for (RenderType target : arcSet.targets(source)) {
+                            graph.remove(source, target);
+                        }
+                    }
+                }
+            }
+
+            if (graph.getVertexCount() == 0) {
+                continue;
+            }
+
+            List<RenderType> renderTypesInReverseOrder = Digraphs.toposort(graph, true);
+
+            // Add in reverse order
+            for (int i = renderTypesInReverseOrder.size() - 1; i >= 0; i--) {
+                allLayers.add(renderTypesInReverseOrder.get(i));
+            }
 		}
 
 		return allLayers;
