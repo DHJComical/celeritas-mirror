2e43940892419388bf96c82af8097734d6471de1
embeddedt <42941056+embeddedt@users.noreply.github.com>
2025-02-14 11:12:39 -0500

    Enable debug in dev by default

--- modern/src/main/java/org/embeddedt/embeddium/impl/mixin/core/RenderSystemMixin.java
+++ modern/src/main/java/org/embeddedt/embeddium/impl/mixin/core/RenderSystemMixin.java
@@ -2,6 +2,7 @@ package org.embeddedt.embeddium.impl.mixin.core;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import org.embeddedt.embeddium.impl.gl.debug.GLDebug;
+import org.embeddedt.embeddium.impl.util.PlatformUtil;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,6 +12,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class RenderSystemMixin {
     @Inject(method = "initRenderer", at = @At("RETURN"))
     private static void resetDebug(CallbackInfo ci) {
+        if (PlatformUtil.isDevelopmentEnvironment()) {
+            System.setProperty("celeritas.enableGLDebug", "true");
+        }
         GLDebug.reloadDebugState();
     }
 }
