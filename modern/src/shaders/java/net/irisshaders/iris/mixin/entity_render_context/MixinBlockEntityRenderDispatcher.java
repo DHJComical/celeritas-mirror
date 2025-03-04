60c79245c9ddc33203dc8c695ce36ffe335667a2
embeddedt <42941056+embeddedt@users.noreply.github.com>
2025-03-04 15:44:54 -0500

    Revert "Rearrange bytecode to avoid an allocation"
    This reverts commit a17b0313ecee8f179ef210c19b6072d92871da57.

--- modern/src/shaders/java/net/irisshaders/iris/mixin/entity_render_context/MixinBlockEntityRenderDispatcher.java
+++ modern/src/shaders/java/net/irisshaders/iris/mixin/entity_render_context/MixinBlockEntityRenderDispatcher.java
@@ -14,7 +14,6 @@ import net.minecraft.client.renderer.blockentity.BlockEntityRenderDispatcher;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -69,12 +68,7 @@ public class MixinBlockEntityRenderDispatcher {
 	@Inject(method = "render", at = @At(value = "INVOKE", target = RUN_REPORTED, shift = At.Shift.AFTER))
 	private void iris$afterRender(BlockEntity blockEntity, float tickDelta, PoseStack matrix,
 								  MultiBufferSource bufferSource, CallbackInfo ci) {
-		iris$finishRender();
-	}
-
-    @Unique
-    private void iris$finishRender() {
-        CapturedRenderingState.INSTANCE.setCurrentBlockEntity(0);
+		CapturedRenderingState.INSTANCE.setCurrentBlockEntity(0);
         celeritas$outerWrapper.setTargetBufferSource(null);
-    }
+	}
 }
