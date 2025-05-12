c91556958a723a77ff3554fe98d8797789117f70
Ven <tudurap.com@gmail.com>
2025-05-12 13:31:00 +0100

    Expanded GlPrimitiveType enum to cover all supported OpenGL primitives

--- common/src/main/java/org/embeddedt/embeddium/impl/gl/tessellation/GlPrimitiveType.java
+++ common/src/main/java/org/embeddedt/embeddium/impl/gl/tessellation/GlPrimitiveType.java
@@ -1,11 +1,29 @@
 package org.embeddedt.embeddium.impl.gl.tessellation;
 
-import org.lwjgl.opengl.GL20C;
-import org.lwjgl.opengl.GL43C;
+import org.lwjgl.opengl.GL11C;
+import org.lwjgl.opengl.GL32C;
+import org.lwjgl.opengl.GL40C;
 
+/**
+ * An enumeration over the supported OpenGL primitive types.
+ */
 public enum GlPrimitiveType {
-    TRIANGLES(GL20C.GL_TRIANGLES),
-    PATCHES(GL43C.GL_PATCHES);
+    POINTS(GL11C.GL_POINTS),
+
+    LINES(GL11C.GL_LINES),
+    LINE_STRIP(GL11C.GL_LINE_STRIP),
+    LINE_LOOP(GL11C.GL_LINE_LOOP),
+
+    TRIANGLES(GL11C.GL_TRIANGLES),
+    TRIANGLE_STRIP(GL11C.GL_TRIANGLE_STRIP),
+    TRIANGLE_FAN(GL11C.GL_TRIANGLE_FAN),
+
+    LINES_ADJACENCY(GL32C.GL_LINES_ADJACENCY),
+    LINE_STRIP_ADJACENCY(GL32C.GL_LINE_STRIP_ADJACENCY),
+    TRIANGLES_ADJACENCY(GL32C.GL_TRIANGLES_ADJACENCY),
+    TRIANGLE_STRIP_ADJACENCY(GL32C.GL_TRIANGLE_STRIP_ADJACENCY),
+
+    PATCHES(GL40C.GL_PATCHES);
 
     private final int id;
 
