fce0f4af650f6c1da20977e862dc5d26866cceed
Jason Mitchell <mitchej@gmail.com>
2025-05-20 08:08:55 -0700

    Don't remove fabric import

--- modern/src/main/shaders_java/net/irisshaders/iris/gui/screen/ShaderPackScreen.java
+++ modern/src/main/shaders_java/net/irisshaders/iris/gui/screen/ShaderPackScreen.java
@@ -33,6 +33,7 @@ import java.io.InputStream;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
