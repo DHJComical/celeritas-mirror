03228879b5557998650588dcd77de6df754e0a1a
embeddedt <42941056+embeddedt@users.noreply.github.com>
2025-05-31 18:50:15 -0400

    [shaders] fix common frustum wrapper not proxying ViewportProvider

--- common-shaders/src/main/java/net/irisshaders/iris/shadows/frustum/CommonFrustum.java
+++ common-shaders/src/main/java/net/irisshaders/iris/shadows/frustum/CommonFrustum.java
@@ -1,6 +1,8 @@
 package net.irisshaders.iris.shadows.frustum;
 
-public abstract class CommonFrustum {
+import org.embeddedt.embeddium.impl.render.viewport.ViewportProvider;
+
+public abstract class CommonFrustum implements ViewportProvider {
     public void prepare(double camX, double camY, double camZ) {
         // Do nothing by default
     }
--- modern/src/main/shaders_java/net/irisshaders/iris/compat/mc/FrustumWrapper.java
+++ modern/src/main/shaders_java/net/irisshaders/iris/compat/mc/FrustumWrapper.java
@@ -5,9 +5,11 @@ import net.irisshaders.iris.shadows.frustum.MCAABB;
 import net.irisshaders.iris.shadows.frustum.MCFrustum;
 import net.minecraft.client.renderer.culling.Frustum;
 import net.minecraft.world.phys.AABB;
+import org.embeddedt.embeddium.impl.render.viewport.Viewport;
+import org.embeddedt.embeddium.impl.render.viewport.ViewportProvider;
 import org.joml.Matrix4f;
 
-public class FrustumWrapper extends Frustum implements MCFrustum {
+public class FrustumWrapper extends Frustum implements MCFrustum, ViewportProvider {
     private static final Matrix4f emptyProjection = new Matrix4f();
     private static final Matrix4f emptyFrustum = new Matrix4f();
 
@@ -34,4 +36,8 @@ public class FrustumWrapper extends Frustum implements MCFrustum {
         return isVisible((MCAABB) aabb);
     }
 
+    @Override
+    public Viewport sodium$createViewport() {
+        return this.frustum.sodium$createViewport();
+    }
 }
