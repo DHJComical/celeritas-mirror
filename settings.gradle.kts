4685e7cdedfa9609b37d85024ce9605db5039d5b
embeddedt <42941056+embeddedt@users.noreply.github.com>
2025-06-06 20:26:04 -0400

    Allow filtering built subprojects/versions with a Gradle property

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+import dev.kikugie.stonecutter.data.tree.TreeBuilder
+
 pluginManagement {
     repositories {
         mavenLocal()
@@ -32,52 +34,70 @@ rootProject.name = "celeritas"
 include("common")
 include("common-shaders")
 
-if(file("forge1710").exists()) {
+val includedVersionsProp = if(extra.has("target_versions")) extra["target_versions"].toString().split(",") else null
+val includedSubprojectsProp = if(extra.has("target_subprojects")) extra["target_subprojects"].toString().split(",") else null
+
+fun isVersionIncluded(ver: String): Boolean {
+    if (includedVersionsProp == null) {
+        return true
+    }
+
+    val testVer = ver.substringBefore('-')
+
+    return includedVersionsProp.any { stonecutter.eval(testVer, it) }
+}
+
+if(file("forge1710").exists() && isVersionIncluded("1.7.10")) {
     include("forge1710")
 }
-if(file("forge122").exists()) {
-    include("forge122")
+
+fun <T> createStonecutterProject(subprojectFolder: String, versions: List<T>, mcVersionGetter: (version: T) -> String = { v -> v.toString() }, action: TreeBuilder.(versions: List<T>) -> Unit) {
+    if (includedSubprojectsProp != null && !includedSubprojectsProp.contains(subprojectFolder)) {
+        println("Skipping project $subprojectFolder by request")
+        return
+    }
+    if (!file(subprojectFolder).exists()) {
+        return
+    }
+    if (!versions.any { isVersionIncluded(mcVersionGetter.invoke(it)) }) {
+        println("Skipping project $subprojectFolder as it does not contain any desired versions")
+        return
+    }
+    val filteredVersions = versions.filter { versions[0] == it || isVersionIncluded(mcVersionGetter.invoke(it)) }
+    val subprojectPath = ":$subprojectFolder"
+    include(subprojectPath)
     stonecutter {
-        create(":forge122") {
-            centralScript = "build.gradle.kts"
-            versions("1.12.2", "1.10.2")
-            vcsVersion = "1.12.2"
+        create(subprojectPath) {
+            action.invoke(this, filteredVersions)
         }
     }
 }
 
-if(file("babric").exists()) {
-    include("babric")
-    stonecutter {
-        create(":babric") {
-            centralScript = "build.gradle.kts"
-            versions("1.0.0-beta.7.3", "1.0.0-beta.8.1", "1.2.5", "1.7.10")
-            vcsVersion = "1.2.5"
-        }
-    }
+createStonecutterProject("forge122", listOf("1.12.2", "1.10.2")) { versions ->
+    centralScript = "build.gradle.kts"
+    versions(versions)
 }
 
-if(file("modern").exists()) {
-    include("modern")
-    data class CeleritasTarget(val friendlyName: String, val loaders: List<String>, val semanticName: String = friendlyName)
-    val targets = listOf(
-            CeleritasTarget("1.16.5", listOf("forge")),
-            CeleritasTarget("1.18.2", listOf("forge")),
-            CeleritasTarget("1.20.1", listOf("forge", "fabric")),
-            //CeleritasTarget("1.20.4", listOf("neoforge")),
-            CeleritasTarget("1.21.1", listOf("fabric", "neoforge")),
-            //CeleritasTarget("1.19.2", listOf("forge", "fabric"))
-    )
-    stonecutter {
-        centralScript = "build.gradle"
-        create(":modern") {
-            targets.forEach {
-                val target = it
-                it.loaders.forEach { loader ->
-                    vers(target.friendlyName + "-" + loader, target.semanticName)
-                }
-            }
-            vcsVersion = "1.20.1-forge"
+createStonecutterProject("babric", listOf("1.2.5", "1.0.0-beta.7.3", "1.0.0-beta.8.1", "1.7.10")) { versions ->
+    centralScript = "build.gradle.kts"
+    versions(versions)
+}
+
+data class CeleritasTarget(val friendlyName: String, val loaders: List<String>, val semanticName: String = friendlyName)
+
+createStonecutterProject("modern", listOf(
+        CeleritasTarget("1.20.1", listOf("forge", "fabric")),
+        CeleritasTarget("1.16.5", listOf("forge")),
+        CeleritasTarget("1.18.2", listOf("forge")),
+        //CeleritasTarget("1.20.4", listOf("neoforge")),
+        CeleritasTarget("1.21.1", listOf("fabric", "neoforge")),
+        //CeleritasTarget("1.19.2", listOf("forge", "fabric"))
+), { it.friendlyName }) { targets ->
+    centralScript = "build.gradle"
+    targets.forEach {
+        val target = it
+        it.loaders.forEach { loader ->
+            vers(target.friendlyName + "-" + loader, target.semanticName)
         }
     }
 }
