c56825ae8a982aebf182cad582ceba0e9ef6fd07
embeddedt <42941056+embeddedt@users.noreply.github.com>
2025-07-31 19:54:15 -0400

    Implement render pass consolidation on postmodern

--- modern/src/main/java/org/embeddedt/embeddium/impl/modern/render/chunk/config/PostmodernRenderPassConfigurationBuilder.java
+++ modern/src/main/java/org/embeddedt/embeddium/impl/modern/render/chunk/config/PostmodernRenderPassConfigurationBuilder.java
@@ -10,6 +10,7 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.textures.GpuTextureView;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.chunk.ChunkSectionLayer;
+import org.embeddedt.embeddium.impl.Celeritas;
 import org.embeddedt.embeddium.impl.mixin.core.render.blaze.GlCommandEncoderAccessor;
 import org.embeddedt.embeddium.impl.render.chunk.RenderPassConfiguration;
 import org.embeddedt.embeddium.impl.render.chunk.compile.sorting.QuadPrimitiveType;
@@ -22,7 +23,50 @@ import org.lwjgl.opengl.GL32C;
 import java.util.*;
 
 public class PostmodernRenderPassConfigurationBuilder {
+    private static final Map<ChunkSectionLayer, ChunkSectionLayer> CONSOLIDATION_MAPPINGS = Map.of(
+            ChunkSectionLayer.CUTOUT, ChunkSectionLayer.CUTOUT_MIPPED
+    );
     private static RenderPass currentChunkRenderPass;
+
+    private final Map<ChunkSectionLayer, Material> layerToMaterial = new EnumMap<>(ChunkSectionLayer.class);
+    private final Map<ChunkSectionLayer, TerrainRenderPass> passForLayer = new EnumMap<>(ChunkSectionLayer.class);
+    private final Map<ChunkSectionLayer, Collection<TerrainRenderPass>> renderStages = new EnumMap<>(ChunkSectionLayer.class);
+    private final Map<ChunkSectionLayer, ChunkSectionLayer> consolidationMap = Celeritas.options().performance.useRenderPassConsolidation ? CONSOLIDATION_MAPPINGS : Map.of();
+    private final ChunkVertexType vertexType;
+
+    public PostmodernRenderPassConfigurationBuilder(ChunkVertexType vertexType) {
+        this.vertexType = vertexType;
+    }
+
+    private TerrainRenderPass buildTerrainRenderPassForLayer(ChunkSectionLayer l) {
+        return this.passForLayer.computeIfAbsent(l, layer -> {
+            var pipeline = layer.pipeline();
+            return TerrainRenderPass.builder().name(layer.name().toLowerCase(Locale.ROOT))
+                    .pipelineState(new PostmodernPipelineState(layer, pipeline))
+                    .vertexType(vertexType)
+                    .primitiveType(QuadPrimitiveType.TRIANGULATED)
+                    .fragmentDiscard(pipeline.getShaderDefines().values().containsKey("ALPHA_CUTOUT"))
+                    .useReverseOrder(layer.sortOnUpload())
+                    .useTranslucencySorting(layer.sortOnUpload())
+                    .build();
+        });
+    }
+
+    public RenderPassConfiguration<ChunkSectionLayer> build() {
+        for (var layer : ChunkSectionLayer.values()) {
+            var pipeline = layer.pipeline();
+            var alphaCutoff = Optional.ofNullable(pipeline.getShaderDefines().values().get("ALPHA_CUTOUT")).map(Float::parseFloat);
+            TerrainRenderPass terrainPass = this.buildTerrainRenderPassForLayer(consolidationMap.getOrDefault(layer, layer));
+            var material = new Material(terrainPass, alphaCutoff.map(AlphaCutoffParameter::valueOf).orElse(AlphaCutoffParameter.ZERO), layer.useMipmaps);
+            layerToMaterial.put(layer, material);
+            renderStages.put(layer, List.of(terrainPass));
+        }
+
+        return new RenderPassConfiguration<>(layerToMaterial, renderStages,
+                layerToMaterial.get(ChunkSectionLayer.SOLID),
+                layerToMaterial.get(ChunkSectionLayer.CUTOUT_MIPPED),
+                layerToMaterial.get(ChunkSectionLayer.TRANSLUCENT));
+    }
 
     private record PostmodernPipelineState(ChunkSectionLayer layer, RenderPipeline vanillaPipeline) implements TerrainRenderPass.PipelineState {
         @Override
@@ -75,30 +119,6 @@ public class PostmodernRenderPassConfigurationBuilder {
             currentChunkRenderPass.close();
             currentChunkRenderPass = null;
         }
-    }
-
-    public static RenderPassConfiguration<ChunkSectionLayer> build(ChunkVertexType vertexType) {
-        Map<ChunkSectionLayer, Material> layerToMaterial = new EnumMap<>(ChunkSectionLayer.class);
-        Map<ChunkSectionLayer, Collection<TerrainRenderPass>> renderStages = new EnumMap<>(ChunkSectionLayer.class);
-        for (var layer : ChunkSectionLayer.values()) {
-            var pipeline = layer.pipeline();
-            var alphaCutoff = Optional.ofNullable(pipeline.getShaderDefines().values().get("ALPHA_CUTOUT")).map(Float::parseFloat);
-            var terrainPass = TerrainRenderPass.builder().name(layer.name().toLowerCase(Locale.ROOT))
-                    .pipelineState(new PostmodernPipelineState(layer, pipeline))
-                    .vertexType(vertexType)
-                    .primitiveType(QuadPrimitiveType.TRIANGULATED)
-                    .fragmentDiscard(alphaCutoff.isPresent())
-                    .useReverseOrder(layer.sortOnUpload())
-                    .useTranslucencySorting(layer.sortOnUpload())
-                    .build();
-            var material = new Material(terrainPass, alphaCutoff.map(AlphaCutoffParameter::valueOf).orElse(AlphaCutoffParameter.ZERO), layer.useMipmaps);
-            layerToMaterial.put(layer, material);
-            renderStages.put(layer, List.of(terrainPass));
-        }
-        return new RenderPassConfiguration<>(layerToMaterial, renderStages,
-                layerToMaterial.get(ChunkSectionLayer.SOLID),
-                layerToMaterial.get(ChunkSectionLayer.CUTOUT_MIPPED),
-                layerToMaterial.get(ChunkSectionLayer.TRANSLUCENT));
     }
 }
 *///?}
