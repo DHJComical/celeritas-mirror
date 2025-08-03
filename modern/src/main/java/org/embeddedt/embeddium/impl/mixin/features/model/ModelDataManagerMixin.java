fba45700e98d1fb0354f566a8fd3a524a9ea7996
embeddedt <42941056+embeddedt@users.noreply.github.com>
2025-08-03 18:24:20 -0400

    Optimize ModelDataManager to do nothing for BEs without model data

--- modern/src/main/java/org/embeddedt/embeddium/impl/mixin/features/model/ModelDataManagerMixin.java
+++ modern/src/main/java/org/embeddedt/embeddium/impl/mixin/features/model/ModelDataManagerMixin.java
@@ -5,15 +5,26 @@ package org.embeddedt.embeddium.impl.mixin.features.model;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraftforge.client.model.data.ModelData;
 import net.minecraftforge.client.model.data.ModelDataManager;
+import org.embeddedt.embeddium.impl.model.BlockEntityImplInfo;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Map;
 
 @Mixin(ModelDataManager.class)
 public class ModelDataManagerMixin {
+    @Inject(method = "requestRefresh", at = @At("HEAD"), cancellable = true)
+    private void celeritas$skipIfNotModelDataProvider(BlockEntity be, CallbackInfo ci) {
+        if (!BlockEntityImplInfo.providesModelData(be)) {
+            ci.cancel();
+        }
+    }
+
     /**
      * @author embeddedt
      * @reason Avoid inserting ModelData.EMPTY into the map, as it's redundant.
@@ -27,4 +38,4 @@ public class ModelDataManagerMixin {
         }
     }
 }
-//? }
+//?}
