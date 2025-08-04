84b215e6b7655990f44815ea4e28d84a85bfc179
embeddedt <42941056+embeddedt@users.noreply.github.com>
2025-08-04 14:46:41 -0400

    Allow CyclingControl to work with things that aren't enums

--- modern/src/main/gui_java/org/embeddedt/embeddium/api/options/control/CyclingControl.java
+++ modern/src/main/gui_java/org/embeddedt/embeddium/api/options/control/CyclingControl.java
@@ -9,47 +9,50 @@ import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
-import org.apache.commons.lang3.Validate;
 
-public class CyclingControl<T extends Enum<T>> implements Control<T> {
+public class CyclingControl<T> implements Control<T> {
     private final Option<T> option;
     private final T[] allowedValues;
     private final Component[] names;
 
     public CyclingControl(Option<T> option, Class<T> enumType) {
-        this(option, enumType, enumType.getEnumConstants());
+        this(option, enumType.getEnumConstants(), determineNames(enumType.getEnumConstants()));
     }
 
     public CyclingControl(Option<T> option, Class<T> enumType, Component[] names) {
-        T[] universe = enumType.getEnumConstants();
-
-        Validate.isTrue(universe.length == names.length, "Mismatch between universe length and names array length");
-        Validate.notEmpty(universe, "The enum universe must contain at least one item");
-
-        this.option = option;
-        this.allowedValues = universe;
-        this.names = names;
+        this(option, enumType.getEnumConstants(), names);
     }
 
     public CyclingControl(Option<T> option, Class<T> enumType, T[] allowedValues) {
-        T[] universe = enumType.getEnumConstants();
+        this(option, allowedValues, determineNames(allowedValues));
+    }
 
+    public CyclingControl(Option<T> option, T[] allowedValues, Component[] names) {
         this.option = option;
+        if (allowedValues.length != names.length) {
+            throw new IllegalArgumentException();
+        }
         this.allowedValues = allowedValues;
-        this.names = new Component[universe.length];
+        this.names = names;
+    }
 
-        for (int i = 0; i < this.names.length; i++) {
+    private static Component[] determineNames(Object[] universe) {
+        Component[] names = new Component[universe.length];
+        for (int i = 0; i < names.length; i++) {
             Component name;
-            T value = universe[i];
+            Object value = universe[i];
 
             if (value instanceof TextProvider) {
                 name = ((TextProvider) value).getLocalizedName();
+            } else if (value instanceof Enum<?> e) {
+                name = ComponentUtil.literal(e.name());
             } else {
-                name = ComponentUtil.literal(value.name());
+                throw new IllegalArgumentException("Could not figure out name of object " + value);
             }
 
-            this.names[i] = name;
+            names[i] = name;
         }
+        return names;
     }
 
     public Component[] getNames() {
@@ -71,7 +74,7 @@ public class CyclingControl<T extends Enum<T>> implements Control<T> {
         return 70;
     }
 
-    private static class CyclingControlElement<T extends Enum<T>> extends ControlElement<T> {
+    private static class CyclingControlElement<T> extends ControlElement<T> {
         private final T[] allowedValues;
         private final Component[] names;
 
@@ -96,8 +99,7 @@ public class CyclingControl<T extends Enum<T>> implements Control<T> {
         public void render(GuiGraphics drawContext, int mouseX, int mouseY, float delta) {
             super.render(drawContext, mouseX, mouseY, delta);
 
-            Enum<T> value = this.option.getValue();
-            Component name = this.names[value.ordinal()];
+            Component name = this.names[getCurrentIndex()];
 
             if(!this.option.isAvailable()) {
                 name = ComponentUtil.empty().append(name).withStyle(ChatFormatting.GRAY, ChatFormatting.STRIKETHROUGH);
