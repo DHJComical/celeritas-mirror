e22b72134ae6d29291f91f4e6536431506e81022
embeddedt <42941056+embeddedt@users.noreply.github.com>
2025-09-06 10:18:34 -0400

    Fix GL buffer handle not being deleted

--- common/src/main/java/org/embeddedt/embeddium/impl/render/chunk/multidraw/IndirectMultiDrawEmitter.java
+++ common/src/main/java/org/embeddedt/embeddium/impl/render/chunk/multidraw/IndirectMultiDrawEmitter.java
@@ -107,5 +107,6 @@ public class IndirectMultiDrawEmitter implements MultiDrawEmitter {
     @Override
     public void delete() {
         MemoryUtil.nmemFree(this.indirectBuffer);
+        this.indirectBufferGpu.delete();
     }
 }
