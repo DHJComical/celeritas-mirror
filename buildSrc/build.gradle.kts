50df6ce3eff3371e5189c9d5c0940ba53942db25
embeddedt <42941056+embeddedt@users.noreply.github.com>
2025-12-08 21:20:10 -0500

    Update Stonecutter

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,7 +12,7 @@ dependencies {
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("org.apache.commons:commons-compress:1.26.0")
     implementation("com.gradleup.shadow:shadow-gradle-plugin:8.3.0")
-    implementation("dev.kikugie:stonecutter:0.7.8")
+    implementation("dev.kikugie:stonecutter:0.7.11")
 }
 
 repositories {
