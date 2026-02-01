0cd4c282a26993792b029dbc12efb4eefac9d3cf
embeddedt <42941056+embeddedt@users.noreply.github.com>
2026-02-01 15:49:12 -0500

    Upgrade Loom & Ploceus

--- ornithe/build.gradle.kts
+++ ornithe/build.gradle.kts
@@ -5,8 +5,8 @@ import java.net.URI
 
 plugins {
     id("celeritas.platform-conventions")
-    id("net.fabricmc.fabric-loom-remap") version "1.14.7"
-    id("ploceus") version "1.14.3"
+    id("net.fabricmc.fabric-loom-remap") version "1.15.3"
+    id("ploceus") version "1.15.6"
 }
 
 repositories {
