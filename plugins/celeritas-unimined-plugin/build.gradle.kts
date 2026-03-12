c65958cd28a9a8684916886d6dd85e19ba57743e
embeddedt <42941056+embeddedt@users.noreply.github.com>
2026-03-12 19:24:12 -0400

    Fix one more NeoForge reference

--- plugins/celeritas-unimined-plugin/build.gradle.kts
+++ plugins/celeritas-unimined-plugin/build.gradle.kts
@@ -11,7 +11,7 @@ dependencies {
 repositories {
     mavenCentral()
     exclusiveContent {
-        forRepository { maven("https://maven.neoforged.net/") }
+        forRepository { maven("https://maven.neoforged.net/releases") }
         filter {
             includeGroup("net.neoforged")
             includeGroup("net.minecraftforge")
